359b45311efce9f760faa1aacf84c49d060e1b40
Prashant Yadav <prashant_yadav@external.mckinsey.com>
2018-03-10 23:01:56 +0530

    firebase configuration added, bootstrap added and created navbar-component

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAwWbpZZA6h0ht8OzZjZ30hme4sYkmMPvM',
+    authDomain: 'shopping-cart-91e36.firebaseapp.com',
+    databaseURL: 'https://shopping-cart-91e36.firebaseio.com',
+    projectId: 'shopping-cart-91e36',
+    storageBucket: 'shopping-cart-91e36.appspot.com',
+    messagingSenderId: '1090951323982'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAwWbpZZA6h0ht8OzZjZ30hme4sYkmMPvM',
+    authDomain: 'shopping-cart-91e36.firebaseapp.com',
+    databaseURL: 'https://shopping-cart-91e36.firebaseio.com',
+    projectId: 'shopping-cart-91e36',
+    storageBucket: 'shopping-cart-91e36.appspot.com',
+    messagingSenderId: '1090951323982'
+  }
 };
